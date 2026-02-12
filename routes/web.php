9f91f9b34c4d0ac13edb2bafbf19ec41885ce0e1
Maria Sofia Oyola Lozano <moyola@unab.edu.co>
2026-02-12 11:57:45 -0500

    php basico

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,54 @@ Route::get('/mi-nombre', function(){
 });
 
 Route::get('/php-basico', function(){
-    return "Sofi";
+
+    echo "<h1 style = 'color: red;'> Aprendiendo PHP </h1>";
+
+    $name = "Sofía Oyola";
+    $height = 1.6;
+    $isLogic = true;
+    $age = 18;
+
+    echo "<h3>  " . $name . "</h3>";
+
+    echo "<br><br> *****ESTRUCTURAS DE CONTROL*****<br><br>";
+
+    $mensaje = "Soy $name, tengo $age años. ". valadateAge($age);
+    $mensaje .= $isLogic ? "Estoy logueado":"no estoy logueado";
+
+    echo $mensaje;
+
+    echo "<br><br> *****ESTRUCTURAS DE DATOS*****<br><br>";
+    
+    //Diccionarios 
+    $pc = [
+        "nombre" => "pc Gamer core9",
+        "price" => 6000,
+        "marca" => "Lenovo"
+    ];
+
+    $teclado = [
+        "nombre" => "Teclado",
+        "price" => 200,
+        "marca" => "Aus"
+    ];
+
+    $listaProductos = [$pc, $teclado];
+
+    //For
+    foreach($listaProductos as $item){
+        echo $item['nombre'] . "<br>";
+    }
+
 });
+
+function valadateAge($age){
+
+    if($age >= 18){
+        return "Soy mayor de edad <br>"; 
+    }else{
+        return "Soy menor de edad <br>";
+    }
+
+};
+
